4c60ccb1a912fdd86d512e9225d4f64250b98bad
Nathan Machoka <natemachoka@gmail.com>
2024-07-28 03:02:01 -0700

    improve engines

--- models/base_model.py
+++ models/base_model.py
@@ -13,29 +13,33 @@ class BaseModel:
     """A base class for all hbnb models"""
 
     id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
         else:
             for k, v in kwargs.items():
                 if k == 'created_at' or k == 'updated_at':
                     v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
                 if k != '__class__':
                     setattr(self, k, v)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.utcnow()
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.utcnow()
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
-        self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -58,7 +58,7 @@ class DBStorage:
         """Reload data from the database"""
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        self.__session = scoped_session(session_factory)()
+        self.__session = scoped_session(session_factory)
 
     def close(self):
         """Close the current session"""
